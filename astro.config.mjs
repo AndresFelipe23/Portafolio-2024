9348446f31af83ae1898088e9b3dcb8b58fa0ff4
Andres Espitia <75282613+AndresFelipe23@users.noreply.github.com>
2024-10-19 15:09:12 -0500

    portafolio
    portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-
+import vercel from '@astrojs/vercel/serverless';
 // Configuración de Astro
 export default defineConfig({
   integrations: [
     tailwind(), 
     icon(),
   ],
-  // Modo de salida híbrido para SSR y prerender
+  adapter: vercel(),
   output: 'hybrid', // Puedes cambiarlo a 'static' o 'server' según tu preferencia
 });
