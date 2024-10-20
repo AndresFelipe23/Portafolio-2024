e6c68ffb5ebf60b2088ed2b4e3d77ff3ec1c17f7
Andres Espitia <75282613+AndresFelipe23@users.noreply.github.com>
2024-10-20 13:28:40 -0500

    mejoras de diseño
    mejoras de diseño

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import vercel from '@astrojs/vercel/serverless';
+
 // Configuración de Astro
 export default defineConfig({
   integrations: [
-    tailwind(), 
-    icon(),
+    tailwind(),
+    icon(), // No especificas "collections" aquí, lo manejarás en tu código
   ],
   adapter: vercel(),
   output: 'hybrid', // Puedes cambiarlo a 'static' o 'server' según tu preferencia
